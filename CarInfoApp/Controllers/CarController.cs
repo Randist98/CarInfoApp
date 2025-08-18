847bfc8c61f663c1d12c3a760207a079030ac381
Rand Kharoush <rand.kharoush@gmail.com>
2025-08-18 21:48:54 +0300

    Add CarController and Views, update _Layout.cshtml

---
+++ CarInfoApp/Controllers/CarController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarInfoApp.Service;
+
+public class CarController : Controller
+{
+    private readonly CarService _carService;
+
+    public CarController(CarService carService)
+    {
+        _carService = carService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var makes = await _carService.GetAllMakes();
+        return View(makes);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> VehicleTypes(int makeId)
+    {
+        var types = await _carService.GetVehicleTypes(makeId);
+        return Json(types);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Models(int makeId, int year)
+    {
+        var models = await _carService.GetModels(makeId, year);
+        return Json(models);
+    }
+}
