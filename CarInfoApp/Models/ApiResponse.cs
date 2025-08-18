82bf13d4a51e9de0018644f85d3ec46bb3ad4085
Rand Kharoush <rand.kharoush@gmail.com>
2025-08-18 15:52:09 +0300

    Add Models

---
+++ CarInfoApp/Models/ApiResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CarInfoApp.Models
+{
+    public class ApiResponse<T>
+    {
+        public List<T> Results { get; set; }
+    }
+}
---
+++ CarInfoApp/Models/CarModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarInfoApp.Models
+{
+    public class CarModel
+    {
+        public int Model_ID { get; set; }
+        public string Model_Name { get; set; }
+    }
+}
